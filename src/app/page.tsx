e60fec3d749bd491e5e8c93937a50f5f507156f7
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2023-06-10 19:17:09 -0300

    feat: creating explorer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,97 @@
-import Image from 'next/image'
+import { ethers } from 'ethers'
 import styles from './page.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+const providerUrl = `https://eth-mainnet.g.alchemy.com/v2/SAxzRau7bQi55UAZ-6EwYXh86FPEwhKE`
+const baseUrl = `https://etherscan.io/`
+// const providerUrl = `https://polygon-mumbai.g.alchemy.com/v2/RI8JMKbVFeCJ-XxtNwCB-A1Eh2e70385`
+// const baseUrl = `https://mumbai.polygonscan.com/`
+
+async function getData(limit: number) {
+  const provider = new ethers.JsonRpcProvider(providerUrl)
+  const blockNumber = await provider.getBlockNumber()
+  const getBlocks = Array.from({ length: limit }, (_, y) => provider.getBlock(blockNumber - (y + 1)))
+  const blocks = await Promise.all(getBlocks);
+
+  const transactionList = blocks[0]?.transactions.slice(0, limit) as string[];
+  const getTransactions = transactionList.map(tx => provider.getTransaction(tx))
+
+  const transactions = await Promise.all(getTransactions)
+
+  return { blocks, transactions };
+
+}
+
+const formatDate = (date: number) => new Date(date * 1000).toLocaleDateString("en", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+})
+
+const shortenWallet = (address = "") => `${address?.slice(0, 18)}...`
+const roundNumber = (value: ethers.BigNumberish) => parseFloat(ethers.formatEther(value)).toFixed(4)
+
+export default async function Home() {
+  const limit = 10
+  const { blocks, transactions } = await getData(limit);
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1 className={styles.logo}>Blockchain Explore</h1>
+      <div className={styles.grid}>
+        <div className={styles.box}>
+          <h3 className={styles.boxTitle}>Last Blocks ({blocks.length})</h3>
+          <ul>
+            {blocks?.map(block =>
+            (<li key={block?.hash}>
+              <div>
+                <Link href={`${baseUrl}/block/${block?.number}`} target="_blank">
+                  {block?.number.toString()}
+                </Link>
+                <br />
+                {formatDate(block?.timestamp as number)}
+              </div>
+              <div>
+                Validater by:{' '}
+                <Link href={`${baseUrl}/address/${block?.miner}`} target="_blank">
+                  {shortenWallet(block?.miner)}
+                </Link>
+                <br />
+                <Link href={`${baseUrl}/txs?block=${block?.number}`} target="_blank">
+                  {block?.length} txns
+                </Link>
+              </div>
+            </li>)
+            )}
+          </ul>
+        </div>
+        <div className={styles.box}>
+          <h3 className={styles.boxTitle}>Last Transactions ({transactions.length})</h3>
+          <ul>
+            {transactions?.map(tx =>
+            (<li key={tx?.hash}>
+              <Link href={`${baseUrl}/tx/${tx?.hash}`} target="_blank">
+                {shortenWallet(tx?.hash)}
+              </Link>
+              <div>
+                From:{' '}
+                <Link href={`${baseUrl}/address/${tx?.from}`} target="_blank">
+                  {shortenWallet(tx?.from)}
+                </Link>
+                <br />
+                To:{' '}
+                <Link href={`${baseUrl}/address/${tx?.to}`} target="_blank">
+                  {shortenWallet(tx?.to as string)}
+                </Link>
+              </div>
+              <div>
+                {roundNumber(tx?.value as bigint)}
+              </div>
+            </li>)
+            )}
+          </ul>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h4 className={styles.footerTitle}>Development by Jeftar Mascarenhas</h4>
     </main>
   )
 }
