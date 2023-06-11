00d7079bf6be04203c08259de07b0972689cf1a7
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2023-06-11 14:35:41 -0300

    feat: changing ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,27 @@ import { ethers } from "ethers";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-const providerUrl = `https://eth-mainnet.g.alchemy.com/v2/SAxzRau7bQi55UAZ-6EwYXh86FPEwhKE`;
 const baseUrl = `https://etherscan.io/`;
-// const providerUrl = `https://polygon-mumbai.g.alchemy.com/v2/RI8JMKbVFeCJ-XxtNwCB-A1Eh2e70385`
 // const baseUrl = `https://mumbai.polygonscan.com/`
 
 async function getData(limit: number) {
-  const provider = new ethers.JsonRpcProvider(providerUrl);
-  const blockNumber = await provider.getBlockNumber();
-  const getBlocks = Array.from({ length: limit }, (_, y) =>
-    provider.getBlock(blockNumber - (y + 1))
-  );
-  const blocks = await Promise.all(getBlocks);
-
-  const transactionList = blocks[0]?.transactions.slice(0, limit) as string[];
-  const getTransactions = transactionList.map((tx) =>
-    provider.getTransaction(tx)
-  );
-
-  const transactions = await Promise.all(getTransactions);
+  const blocks = [
+    {
+      hash: "0xff9216bb167fb7a5657e9c02ce88f3ef8dab304fe18bc378e87e0ea13bdc003c",
+      number: 17453024,
+      timestamp: 1686439811,
+      miner: "0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5",
+      length: 119,
+    },
+  ];
+  const transactions = [
+    {
+      hash: "0x933de60e0e95ff99a08302b4b3464892d57da419e253758e025ce82b12acb1c7",
+      from: "0x22F6CC8738308a8c92a6a71ea67832463d1Fec0d",
+      to: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      value: BigInt("994901480631357861"),
+    },
+  ];
 
   return { blocks, transactions };
 }
@@ -39,24 +41,8 @@ const roundNumber = (value: ethers.BigNumberish) =>
 
 export default async function Home() {
   const limit = 10;
-  // const { blocks, transactions } = await getData(limit);
-  const blocks = [
-    {
-      hash: "0xff9216bb167fb7a5657e9c02ce88f3ef8dab304fe18bc378e87e0ea13bdc003c",
-      number: 17453024,
-      timestamp: 1686439811,
-      miner: "0xDAFEA492D9c6733ae3d56b7Ed1ADB60692c98Bc5",
-      length: 119,
-    },
-  ];
-  const transactions = [
-    {
-      hash: "0x933de60e0e95ff99a08302b4b3464892d57da419e253758e025ce82b12acb1c7",
-      from: "0x22F6CC8738308a8c92a6a71ea67832463d1Fec0d",
-      to: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-      value: BigInt("994901480631357861"),
-    },
-  ];
+  const { blocks, transactions } = await getData(limit);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.logo}>Blockchain Explore</h1>
@@ -117,13 +103,23 @@ export default async function Home() {
                     {shortenWallet(tx?.to as string)}
                   </Link>
                 </div>
-                <div>{roundNumber(tx?.value)}</div>
+                <div className={styles.tag}>
+                  {roundNumber(tx?.value as bigint)} <strong>eth</strong>
+                </div>
               </li>
             ))}
           </ul>
         </div>
       </div>
-      <h4 className={styles.footerTitle}>Development by Jeftar Mascarenhas</h4>
+      <h4 className={styles.footerTitle}>
+        Development by{" "}
+        <Link
+          href="https://www.linkedin.com/in/jeftarmascarenhas/"
+          target="_blank"
+        >
+          Jeftar Mascarenhas
+        </Link>
+      </h4>
     </main>
   );
 }
